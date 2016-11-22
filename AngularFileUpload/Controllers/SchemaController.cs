10b0de9df577d8bc609256c69d0fb3f334fe590d
v-mingpw <v-mingpw@V-MINGPW-03>
2016-11-22 11:25:50 +0800

    Add project files.

---
+++ AngularFileUpload/Controllers/SchemaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularFileUpload.Controllers
+{
+    public class SchemaController : Controller
+    {
+        // GET: Schema
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
